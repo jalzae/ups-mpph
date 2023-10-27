520d16fdd3cc3a42abc44b73682b1cbe705ae642
JalzAe <jalzae@gmail.com>
2023-10-27 18:49:46 +0700

    update logo dan alert

--- components/Global/Loading.vue
+++ components/Global/Loading.vue
@@ -3,7 +3,7 @@
     <div class="textLoader">
       <div class="text-center items-center">
         <img
-          :src="`/images/logo.png`"
+          :src="`/logo.png`"
           alt="navbar brand"
           class="navbar-brand"
           style="min-height: 100%"
