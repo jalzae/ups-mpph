8e466807547c560a32c927c11796d55e99562c49
jalzae@gmail.com <jalzae@gmail.com>
2023-10-27 17:02:57 +0700

    update tugas.vue

--- pages/tugas.vue
+++ pages/tugas.vue
@@ -21,6 +21,7 @@
         type="text"
         class="custom-input border border-gray-300 rounded outline-none p-2 w-full mt-2"
         v-model="keyword"
+        placeholder="Search name .."
       />
     </div>
     <Tables
